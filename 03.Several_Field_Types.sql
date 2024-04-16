e660d2215cceef1c316eb3a08e855b40a6de4ec1
Román González Díez <ro.github@gmail.com>
2024-04-16 18:09:49 +0200

    Creación y documentación de diferentes tipos de datos

---
+++ 03.Several_Field_Types.sql
@@ -0,0 +1,42 @@
+ALTER TABLE Test ADD 
+	charcol CHAR(1),
+	varcharcol VARCHAR(1),
+	textcol TEXT,
+	ncharcol NCHAR(1),
+	nvarcharcol NVARCHAR(1),
+	ntextcol NTEXT,
+
+	datecol	DATE,
+	timecol TIME,
+	smalldatetimecol SMALLDATETIME,
+	datetimecol DATETIME,
+	datetimeoffsetcol DATETIMEOFFSET,
+	datetime2col DATETIME2,
+
+	numericcol NUMERIC,
+	bitcol BIT,
+	tinyintcol TINYINT,
+	smallintcol SMALLINT,
+	intcol INT,
+	bigintcol BIGINT,
+	floatcol FLOAT,
+	decimalcol DECIMAL(10,2),
+	smallmoneycol SMALLMONEY,
+	moneycol MONEY,
+
+	binarycol BINARY,
+	varbinarycol VARBINARY,
+	imagecol IMAGE,
+
+	rowversioncol ROWVERSION,
+	hierarchyidcol HIERARCHYID,
+	uniqueidentifiercol UNIQUEIDENTIFIER,
+	sql_variantcol SQL_VARIANT,
+	xmlcol XML,
+	geometrycol GEOMETRY,
+	geographycol GEOGRAPHY
+
+
+
+
+
